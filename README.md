49f169164840e2a5731b44584814bdb33c393384
harshag@00389 <harshadgaikwad14@gmail.com>
2019-10-11 01:24:23 +0530

    code added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # springboot-jwt-mysql-security
 we will be implementing a Spring Boot Project to secure a REST API using JWT. Also we will be implementing MYSQL JPA for storing and fetching user credentials.
+Server version: 5.1.72-community MySQL Community Server (GPL)
---
+++ src/main/java/com/example/demo/SpringbootJwtMysqlSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtMysqlSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtMysqlSecurityApplication.class, args);
+	}
+
+}
